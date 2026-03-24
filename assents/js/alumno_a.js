5a8f1874a5f0835a660efd5b8190955019cab5d2
Elisabete Barbosa <ecrbarbosa7@gmail.com>
2026-03-24 10:39:42 +0100

    inventario

---
+++ assents/js/alumno_a.js
@@ -0,0 +1,20 @@
+const inventarioTienda = [
+  {
+    titulo: "Super Mario",
+    precio: 29.99,
+    esDigital: false,
+    generos: ["Plataformas"]
+  },
+  {
+    titulo: "Sonic",
+    precio: 44.99,
+    esDigital: true,
+    generos: ["Acción"]
+  },
+  {
+    titulo: "Tetris",
+    precio: 9.99,
+    esDigital: true,
+    generos: ["Puzzle"]
+  }
+];
